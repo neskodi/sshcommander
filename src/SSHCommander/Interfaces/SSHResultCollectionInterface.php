a3487ee37a2a1bd83da9eadfaa21a3fe91db5c9a
Serhiy Neskhodovskiy <sn@nom.ua>
2019-12-21 19:53:19 +0200

    Add result collection

---
+++ src/SSHCommander/Interfaces/SSHResultCollectionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Neskodi\SSHCommander\Interfaces;
+
+interface SSHResultCollectionInterface
+{
+
+}
---
+++ src/SSHCommander/SSHResultCollection.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Neskodi\SSHCommander;
+
+class SSHResultCollection
+{
+
+}
